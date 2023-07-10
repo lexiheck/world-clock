ace36a171dbb4fafb20bb6697967d792edee9cbc
lexi <lexiii.heck@gmail.com>
2023-07-10 15:36:47 -0400

    added current location to select element

--- Javascript/index.js
+++ Javascript/index.js
@@ -21,6 +21,9 @@ sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
 
 function updateCity(event){
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current"){
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesELement = document.querySelector("#cities");
